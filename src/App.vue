158a5c22769b77d1ba34b7018f88309d40ea91c5
tamda21 <abdetam452@gmail.com>
2022-11-08 18:10:16 +0100

    intro vue slot

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <form-helpers>
+    <h2 slot="title">{{title}}</h2>
+    <p slot="text">This is Paragraph Form</p>
+  </form-helpers>
+  </div>
+    
+</template>
+
+<script>
+import FormHelpers from './Components/FormHelpers';
+export default {
+  components:{
+    'form-helpers':FormHelpers
+  },
+  data () {
+    return {
+      title:"This is Title Form"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Components/FormHelpers.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>
+   <slot name="title"></slot>
+    <h1>Welcome In form Helper</h1>
+   <slot name="text"></slot>
+ </div>
+</template>
+
+<script>
+export default {
+    
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
